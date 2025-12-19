6eaefbd4e85339690835fcaff7d4e14f4dbb74c0
Wida <itswithermc@gmail.com>
2025-12-19 10:13:21 +0700

    giảng viên model

---
+++ Models/GiangVien.cs
@@ -0,0 +1,28 @@
+﻿public class GiangVien { //19
+    public string MaCB { get; set; }
+    public string HoTen { get; set; }
+    public string GioiTinh { get; set; }
+    public DateTime NgaySinh { get; set; }
+
+    public int? TinhId { get; set; }
+    public int? XaId { get; set; }
+    public int? DanTocId { get; set; }
+    public int? TonGiaoId { get; set; }
+
+    public string SDT { get; set; }
+
+    public int? TrinhDoCMId { get; set; }
+    public int? TrinhDoLLCTId { get; set; }
+
+    public string DonViCongTac { get; set; }
+    public int? ChucVuId { get; set; }
+    public int? CapBacId { get; set; }
+
+    public double? HeSoLuong { get; set; }
+
+    public int? ChucDanhId { get; set; }
+    public int? HocHamId { get; set; }
+    public int? HocViId { get; set; }
+
+    public string LinhVucChuyenMon { get; set; }
+}
