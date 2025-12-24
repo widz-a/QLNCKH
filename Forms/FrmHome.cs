71910343783e5a560e7a9f9edd5798a7f26ec901
Wida <itswithermc@gmail.com>
2025-12-24 23:19:46 +0700

    tạm thời home đẹp valll

--- Forms/FrmHome.cs
+++ Forms/FrmHome.cs
@@ -5,17 +5,18 @@ using System.Windows.Forms;
 
 namespace QLNCKH.Forms
 {
-    public partial class FrmHome : Form
-    {
-
-        public FrmHome()
-        {
+    public partial class FrmHome : Form {
+        private FrmMain? _mainForm = null;
+        public FrmHome() {
             InitializeComponent();
         }
 
-        private void btnNopBai_Click(object sender, EventArgs e)
-        {
-            new FrmNopDeTaiEdit().ShowDialog();
+        public FrmHome(FrmMain mainForm) : this() {
+            _mainForm = mainForm;
+        }
+
+        private void btnNopBai_Click(object sender, EventArgs e) {
+            _mainForm.LoadForm(new FrmNopDeTaiEdit(), "Nộp sản phẩm");
         }
     }
 }
