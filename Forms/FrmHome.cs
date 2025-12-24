e4a8a01c38261b2b47703d5ad6bd5c114e43978f
Nongmanhduc <duchajzzkute@gmail.com>
2025-12-24 22:17:10 +0700

    Revert "meow"
    This reverts commit 3f3bd893624f5c338a9cbab40c3c7b46f71a7758.

--- Forms/FrmHome.cs
+++ Forms/FrmHome.cs
@@ -1,21 +1,17 @@
 ﻿using System;
-using System.Configuration;
-using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace QLNCKH.Forms
-{
-    public partial class FrmHome : Form
-    {
-
-        public FrmHome()
-        {
+namespace QLNCKH.Forms {
+    public partial class FrmHome : Form {
+        public FrmHome() {
             InitializeComponent();
-        }
-
-        private void btnNopBai_Click(object sender, EventArgs e)
-        {
-            new FrmNopDeTaiEdit().ShowDialog();
         }
     }
 }
